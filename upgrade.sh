6765372c4cd6c32d2bcec05fcfdfbed9f7b5c624
kitazaki <kitazaki@gmail.com>
2020-06-06 21:29:21 +0900

    Add files via upload

---
+++ upgrade.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+sudo rsync -av /boot/ /media/jetson/239f3029-4825-443b-972e-4392c57f5749/boot/
+sudo rsync -av /lib/modules/4.9.140-tegra/ /media/jetson/239f3029-4825-443b-972e-4392c57f5749/lib/modules/4.9.140-tegra/
+
